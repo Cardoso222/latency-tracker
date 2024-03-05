f7d650e7c449624db125fc300101a2d506a08808
Paulo Henrique <pcardoso@upstart13.com>
2024-03-05 16:52:17 -0300

    Initial Commit

---
+++ LatencyTracker/LatencyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatencyTrackerApp.swift
+//  LatencyTracker
+//
+//  Created by Paulo  Henrique on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LatencyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
